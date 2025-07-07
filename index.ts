5aa96957ac71d20c5552d72d94065b2544d8b030
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-07-08 00:02:30 +0100

    feat: add dotenv for environment configuration

--- index.ts
+++ index.ts
@@ -2,11 +2,21 @@ import Tesseract from "tesseract.js";
 import { franc } from "franc";
 import express, { Request, Response } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+dotenv.config();
 
-app.use(cors());
+const allowedOrigins = process.env.CLIENT_URL
+  ? process.env.CLIENT_URL.split(",").map((url) => url.trim())
+  : ["http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 
 app.post("/extract-text", async (req: Request, res: Response) => {
